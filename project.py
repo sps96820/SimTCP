89643857b660835440ffb8cf08861feac5af3fb4
Sterling <sps96820@uga.edu>
2024-03-02 00:30:37 -0500

    final

--- project.py
+++ project.py
@@ -1,18 +1,23 @@
+"""
+    This code implements a stop n go protocol for handling packet loss
+    over a TCP connection. The TCP connection is simulated by provided
+    files.
+"""
+
+from statistics import mean
 import socket
 import io
 import time
-import typing
 import struct
+import pandas as pd
 import util
 import util.logging
-import pandas as pd
-from statistics import mean
 
 # Constants for packet type
 DATA_PACKET = 0
-ACK_PACKET = 1
-global queue
+ACK_P = 1
 queue = []
+
 
 def send(sock: socket.socket, data: bytes):
     """
@@ -25,17 +30,12 @@ def send(sock: socket.socket, data: bytes):
         data -- A bytes object, containing the data to send over the network.
     """
     logger = util.logging.get_logger("project-sender")
-    chunk_size = util.MAX_PACKET
-    pause = .1
     offsets = range(0, len(data), util.MAX_PACKET - 4)
     sock.settimeout(.05)
     sequence_number = 0
-    timeout = .1
 
     try:
-        for chunk in [data[i:i + chunk_size] for i in offsets]:
-            #print(struct.pack("!HH", DATA_PACKET, sequence_number))
-            #print(chunk)
+        for chunk in [data[i:i + util.MAX_PACKET] for i in offsets]:
             packet = struct.pack("!HH", DATA_PACKET, sequence_number) + chunk
             sock.send(packet)
             logger.info("Sent packet with sequence number %d", sequence_number)
@@ -45,50 +45,35 @@ def send(sock: socket.socket, data: bytes):
             while True:
                 try:
                     ack = sock.recv(util.MAX_PACKET)
-                    #print("in send while")
                     ack_type, ack_sequence = struct.unpack("!HH", ack)
-                    if ack_type == ACK_PACKET and ack_sequence == sequence_number:
+
+                    if len(queue) > 5:
+                        queue.clear()
+
+                    if ack_type == ACK_P and ack_sequence == sequence_number:
                         elapsed_time = time.time() - start_time
                         queue.append(elapsed_time)
-                        RTT_series = pd.Series(queue)
+                        rtt_series = pd.Series(queue)
 
-                        if len(queue) > 10:
-                            queue.clear()
-
-                        moving_averages = round(RTT_series.ewm(
-                            alpha = 1, adjust = True 
+                        moving_averages = round(rtt_series.ewm(
+                            alpha=.3, adjust=True
                         ).mean(), 5)
-
-                        moving_averages_list = moving_averages.tolist()
-                        sock.settimeout(mean(moving_averages_list))
-                        print("\n")
-                        print(mean(moving_averages_list))
-                        print("\n")
-                        logger.info("Received ACK for sequence number %d", sequence_number)
-                        print("\n ack received")
+                        sock.settimeout(mean(moving_averages.tolist()))
+                        logger.info(
+                            "Received ACK for sequence number %d", sequence_number)  # noqa
                         break
                 except socket.timeout:
                     elapsed_time = time.time() - start_time
                     queue.append(elapsed_time)
-                    RTT_series = pd.Series(queue)
+                    rtt_series = pd.Series(queue)
 
-                    moving_averages = round(RTT_series.ewm(
-                        alpha = 0.5, adjust = False 
-                    ).mean(), 2)
-
-                    moving_averages_list = moving_averages.tolist()
-                    sock.settimeout(mean(moving_averages_list))
-                    if elapsed_time >= timeout:
-                        logger.error("Timeout waiting for ACK. Resending packet with sequence number %d", sequence_number)
-                        sock.send(packet)
-                        start_time = time.time()
-                    else:
-                        logger.debug("Still waiting for ACK for sequence number %d", sequence_number)
-                    
+                    moving_averages = round(rtt_series.ewm(
+                        alpha=1, adjust=True
+                    ).mean(), 5)
+                    sock.settimeout(mean(moving_averages.tolist()))
+                    sock.send(packet)
+                    start_time = time.time()
             sequence_number = 1 - sequence_number
-
-            logger.info("Pausing for %f seconds", round(pause, 2))
-            #time.sleep(pause)
     except socket.error as e:
         logger.error("Socket error occurred: %s", str(e))
 
@@ -117,28 +102,31 @@ def recv(sock: socket.socket, dest: io.BufferedIOBase) -> int:
 
             packet_type, sequence_number = struct.unpack("!HH", packet[:4])
 
-            if packet_type == DATA_PACKET and sequence_number == expected_sequence_number:
-                logger.info("Received packet with sequence number %d", sequence_number)
+            if packet_type == DATA_PACKET and sequence_number == expected_sequence_number:  # noqa
+                logger.info(
+                    "Received packet with sequence number %d", sequence_number)
                 dest.write(packet[4:])
                 dest.flush()
                 num_bytes += len(packet) - 4
-                # Send ACK  
-                ack = struct.pack("!HH", ACK_PACKET, sequence_number)
+                # Send ACK
+                ack = struct.pack("!HH", ACK_P, sequence_number)
                 sock.send(ack)
                 logger.info("Sent ACK for sequence number %d", sequence_number)
 
                 expected_sequence_number = 1 - expected_sequence_number
-            elif packet_type == ACK_PACKET:
+            elif packet_type == ACK_P:
                 # Ignore duplicate ACKs
-                logger.info("Received duplicate ACK for sequence number %d", sequence_number)
+                logger.info(
+                    "Received duplicate ACK for sequence number %d", sequence_number)  # noqa
             elif sequence_number != expected_sequence_number:
                 logger.info("duplicate packet found")
-                ack = struct.pack("!HH", ACK_PACKET, sequence_number)
+                ack = struct.pack("!HH", ACK_P, sequence_number)
                 sock.send(ack)
                 logger.info("Sent ACK for sequence number %d", sequence_number)
             else:
                 # Handle packet loss or corruption
-                logger.info("Received out-of-sequence packet or corrupted packet. Ignoring.")
+                logger.info(
+                    "Received out-of-sequence packet or corrupted packet. Ignoring.")  # noqa
         except socket.timeout:
             logger.info("Timed out durring reception")
         except socket.error as e:
